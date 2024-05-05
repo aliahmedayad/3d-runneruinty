e6109cfcd82503829cfd59748ba9e7ad3879744c
Abdelrahman-Eldemerdash <abdelrahman.eldemerdash1@gmail.com>
2024-05-06 02:24:17 +0300

    ai added

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,26 +5,68 @@ using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
-    public NavMeshAgent enmey;
-    public Transform player;
+    public NavMeshAgent enemy;
     public Rigidbody rb;
-    // Start is called before the first frame update
+    public bool playerInSightRange;
+    public float sightRange;
+
+    // Reference to the player object
+    private Transform player;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-       
     }
-    void Start()
 
+    private void Start()
     {
-        
+        // Find the player object dynamically by tag
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (player == null)
+        {
+            Debug.LogError("Player not found! Make sure the player is tagged with 'Player'.");
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 targetPosition = new Vector3(player.position.x, transform.position.y, transform.position.z);
+        if (player == null)
+            return; // Player not found, so don't proceed with the update
 
-        enmey.SetDestination(targetPosition);
+        // Calculate direction and distance to player
+        Vector3 directionToPlayer = player.position - transform.position;
+        float distanceToPlayer = directionToPlayer.magnitude;
+
+        // Check if player is within sight range
+        if (distanceToPlayer <= 10)
+        {
+            playerInSightRange = true;
+        }
+        else
+        {
+            playerInSightRange = false;
+        }
+
+        // If player is within sight range, set destination to player
+        if (playerInSightRange)
+        {
+            Vector3 targetPosition = new Vector3(player.position.x, transform.position.y, transform.position.z);
+            enemy.SetDestination(targetPosition);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "graphic")
+        {
+            Destroy(collision.gameObject);
+            Debug.Log("lol");
+
+        }
+        if (collision.gameObject.name == "coin(Clone)")
+        {
+            
+            Destroy(collision.gameObject);
+
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,51 +15,62 @@ public class PlayerController : MonoBehaviour
     public float IncreaseSpeed;
     [SerializeField] private float jumpforce = 100;
     [SerializeField] private LayerMask GroundMask;
+    private float startYPosition;
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        startYPosition = transform.position.y;
     }
+
     private void FixedUpdate()
     {
         if (Isalive)
         {
-            Vector3 forwardmovment = transform.forward *VerticalInput * Runspeed * Time.fixedDeltaTime;
+            Vector3 forwardmovment = transform.forward * Runspeed * Time.fixedDeltaTime;
             Vector3 horizontalMovment = transform.right * HorizontalInput * Horizontalspeed * Time.fixedDeltaTime;
             rb.MovePosition(rb.position + forwardmovment + horizontalMovment);
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        HorizontalInput = Input.GetAxis("Horizontal"); 
+        HorizontalInput = Input.GetAxis("Horizontal");
         VerticalInput = Input.GetAxis("Vertical");
-        float playerheight =GetComponent<Collider>().bounds.size.y;
+        float playerheight = GetComponent<Collider>().bounds.size.y;
         bool Isground = Physics.Raycast(transform.position, Vector3.down, (playerheight / 2) + 0.1f, GroundMask);
-        if (Input.GetKeyDown(KeyCode.Space)&&Isalive==true) {
-            Console.WriteLine("here");
-            jump();
 
+        // If the player is not on the ground and is alive, and the Y position has decreased significantly from the start
+        if (!Isground && Isalive && transform.position.y < startYPosition )
+        {
+            Dead();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && Isalive && Isground)
+        {
+            jump();
         }
     }
+
     public void jump()
     {
         rb.AddForce(Vector3.up * jumpforce);
         SoundManager.playSound("Jump");
     }
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.name == "graphic")
+        if (collision.gameObject.name == "graphic" || collision.gameObject.name == "Enemy" || collision.gameObject.tag == "obs")
         {
             SoundManager.playSound("Over");
             Dead();
-            Debug.Log("lol");
-
         }
         if (collision.gameObject.name == "coin(Clone)")
         {
@@ -69,9 +80,10 @@ public class PlayerController : MonoBehaviour
             Runspeed += IncreaseSpeed;
         }
     }
+
     private void Dead()
     {
-        Isalive=false;
+        Isalive = false;
         GameManager.Instance.gameOver.SetActive(true);
     }
 }
